5090b59d9fe5a9aaf72d6e7bc23aa535fc13afbd
Clara Asplund <eunjc@ulrik.uio.no>
2023-10-04 22:59:35 +0200

    AspSamllStmtList

--- in2030-oblig-2023/no/uio/ifi/asp/parser/AspSmallStmtList.java
+++ in2030-oblig-2023/no/uio/ifi/asp/parser/AspSmallStmtList.java
@@ -3,12 +3,33 @@ package no.uio.ifi.asp.parser;
 import no.uio.ifi.asp.runtime.RuntimeReturnValue;
 import no.uio.ifi.asp.runtime.RuntimeScope;
 import no.uio.ifi.asp.runtime.RuntimeValue;
+import java.util.ArrayList;
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.runtime.*;
+import no.uio.ifi.asp.scanner.*;
+import static no.uio.ifi.asp.scanner.TokenKind.*;
 
 public class AspSmallStmtList extends AspStmt {
-
+    ArrayList<AspSmallStmt> smallStmts = new ArrayList<>();
+    
     AspSmallStmtList(int n) {
         super(n);
     
+    }
+
+    public static AspSmallStmtList parse(Scanner s) {
+        enterParser("AspSmallStmtList");
+
+        AspSmallStmtList assl = new AspSmallStmtList(s.curLineNum());
+
+        while(true){
+            assl.smallStmts.add(AspSmallStmt.parse(s));
+            skip(s, semicolonToken);
+            if(s.curToken().kind == newLineToken) break;
+        }
+
+        leaveParser("AspSmallStmtList");
+        return assl;
     }
 
     @Override
