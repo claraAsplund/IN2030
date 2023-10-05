50ed0cbe75f743f4c0d2798e62ebbcc5a20a95c6
Clara Asplund <eunjc@ulrik.uio.no>
2023-10-05 16:00:39 +0200

    AspPrimary

--- in2030-oblig-2023/no/uio/ifi/asp/parser/AspPrimary.java
+++ in2030-oblig-2023/no/uio/ifi/asp/parser/AspPrimary.java
@@ -1,14 +1,36 @@
 package no.uio.ifi.asp.parser;
+import java.util.ArrayList;
 
 import no.uio.ifi.asp.runtime.RuntimeReturnValue;
 import no.uio.ifi.asp.runtime.RuntimeScope;
 import no.uio.ifi.asp.runtime.RuntimeValue;
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.runtime.*;
+import no.uio.ifi.asp.scanner.*;
+import static no.uio.ifi.asp.scanner.TokenKind.*;
 
 public class AspPrimary extends AspSyntax{
+    ArrayList<AspPrimarySuffix> suffixs = new ArrayList<>();
+    AspAtom atom;
 
     AspPrimary(int n) {
         super(n);
     
+    }
+    public static AspPrimary parse(Scanner s){
+        
+        enterParser("primary");
+        AspPrimary ap = new AspPrimary(s.curLineNum());
+        
+       
+        while(s.curToken().kind  ==TokenKind.leftParToken || 
+            s.curToken().kind  ==TokenKind.leftBracketToken) {
+                ap.suffixs.add(AspPrimarySuffix.parse(s));
+        }
+
+        leaveParser("primary");
+        return ap;
+
     }
 
     @Override
