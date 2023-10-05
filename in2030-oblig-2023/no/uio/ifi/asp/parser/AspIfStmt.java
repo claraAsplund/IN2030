3a3f9e3536d96053c9183c7690a09b6c69254c6a
Clara Asplund <eunjc@ulrik.uio.no>
2023-10-05 23:20:26 +0200

    ifstmt and suite

--- in2030-oblig-2023/no/uio/ifi/asp/parser/AspIfStmt.java
+++ in2030-oblig-2023/no/uio/ifi/asp/parser/AspIfStmt.java
@@ -1,14 +1,51 @@
 package no.uio.ifi.asp.parser;
 
-import no.uio.ifi.asp.runtime.RuntimeReturnValue;
-import no.uio.ifi.asp.runtime.RuntimeScope;
-import no.uio.ifi.asp.runtime.RuntimeValue;
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.scanner.*;
+import no.uio.ifi.asp.runtime.*;
+import static no.uio.ifi.asp.scanner.TokenKind.*;
 
 public class AspIfStmt extends AspCompoundStmt{
+    AspExpr expr;
+    AspSuite suite;
 
     AspIfStmt(int n) {
         super(n);
     
+    }
+
+    public static AspIfStmt parse(Scanner s){
+        enterParser("if stmt");
+        AspIfStmt ais = new AspIfStmt(s.curLineNum());
+
+        skip(s, ifToken);
+        ais.expr = AspExpr.parse(s);
+        skip(s, colonToken);
+        ais.suite = AspSuite.parse(s);
+
+        while(true){
+            if(s.curToken().kind == elifToken){
+                ais.expr = AspExpr.parse(s);
+                skip(s, colonToken);
+                ais.suite = AspSuite.parse(s);
+
+            }
+            else{
+                break;
+            }
+            
+        }
+
+        if(s.curToken().kind == elseToken){
+            skip(s,colonToken);
+            ais.suite = AspSuite.parse(s);
+        }
+
+        leaveParser("if stmt");
+        return ais;
+
+
+
     }
 
     @Override
--- in2030-oblig-2023/no/uio/ifi/asp/parser/AspSuite.java
+++ in2030-oblig-2023/no/uio/ifi/asp/parser/AspSuite.java
@@ -1,15 +1,48 @@
 package no.uio.ifi.asp.parser;
 
-import no.uio.ifi.asp.runtime.RuntimeReturnValue;
-import no.uio.ifi.asp.runtime.RuntimeScope;
-import no.uio.ifi.asp.runtime.RuntimeValue;
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.scanner.*;
+import no.uio.ifi.asp.runtime.*;
+import static no.uio.ifi.asp.scanner.TokenKind.*;
+
+import java.util.ArrayList;
 
 public class AspSuite extends AspSyntax{
+    ArrayList<AspStmt> stmts = new ArrayList<>();
+    AspSmallStmtList smallStmtList;
 
     AspSuite(int n) {
         super(n);
     
     }
+    public static AspSuite parse(Scanner s){
+        enterParser("suite");
+        AspSuite as = new AspSuite(s.curLineNum());
+
+        if(s.curToken().kind != newLineToken){
+            as.smallStmtList = AspSmallStmtList.parse(s);
+            
+        }
+        else if(s.curToken().kind == newLineToken){
+            skip(s, newLineToken);
+            skip(s, indentToken);
+
+            as.stmts.add(AspStmt.parse(s));
+            while(true){
+                if(s.curToken().kind == dedentToken){
+                    break;
+                }
+                else{
+                    as.stmts.add(AspStmt.parse(s));
+                }
+            }
+        }
+        
+        leaveParser("suite");
+        return as;
+    }
+
+
 
     @Override
     public void prettyPrint(){
