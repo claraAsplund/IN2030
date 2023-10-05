c184021d9a7fd0e42cc3f09183b1b68ea7713bb3
Clara Asplund <eunjc@ulrik.uio.no>
2023-10-05 15:40:19 +0200

    AspPrimarySuffix

--- in2030-oblig-2023/no/uio/ifi/asp/parser/AspPrimarySuffix.java
+++ in2030-oblig-2023/no/uio/ifi/asp/parser/AspPrimarySuffix.java
@@ -3,6 +3,10 @@ package no.uio.ifi.asp.parser;
 import no.uio.ifi.asp.runtime.RuntimeReturnValue;
 import no.uio.ifi.asp.runtime.RuntimeScope;
 import no.uio.ifi.asp.runtime.RuntimeValue;
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.runtime.*;
+import no.uio.ifi.asp.scanner.*;
+import static no.uio.ifi.asp.scanner.TokenKind.*;
 
 abstract class AspPrimarySuffix extends AspSyntax {
 
@@ -10,6 +14,30 @@ abstract class AspPrimarySuffix extends AspSyntax {
         super(n);
     
     }
+    public static AspPrimarySuffix parse(Scanner s){
+        AspPrimarySuffix aps = null;
+        enterParser("primary suffix");
+
+        if(s.curToken().kind == TokenKind.leftParToken){
+            aps = AspArguments.parse(s);
+        }
+        else if (s.curToken().kind == TokenKind.leftBracketToken){
+            aps = AspSubscription.parse(s);
+        } else{
+            parserError("Expected an expression AspPrimarySuffix but found a " + 
+            s.curToken().kind + "!", s.curLineNum());
+
+        }    
+
+        enterParser("AspAndTest");
+        return aps;
+
+    }   
+
+
+    
+
+
 
     @Override
     public void prettyPrint(){
